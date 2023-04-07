96e57de8be061b8011d0e382ac7a476d17f5c51d
hiitssid <see.siddhartha@gmail.com>
2023-04-07 14:57:35 +0545

    added data

--- src/assets/data.js
+++ src/assets/data.js
@@ -45,143 +45,160 @@ export default [
     id: 7,
     name: "Messi",
     interest: "Football",
-    imageurl:"https://upload.wikimedia.org/wikipedia/commons/8/83/Lionel_Messi_31mar2007.jpg",
+    imageurl:
+      "https://upload.wikimedia.org/wikipedia/commons/8/83/Lionel_Messi_31mar2007.jpg",
     dob: { year: 1987, month: 6, date: 24 },
   },
   {
     id: 8,
     name: "Gagan",
     interest: "Politics",
-    imageurl:"https://unitedweblog.files.wordpress.com/2009/11/gagan_thapa_speaking.jpg",
+    imageurl:
+      "https://unitedweblog.files.wordpress.com/2009/11/gagan_thapa_speaking.jpg",
     dob: { year: 1976, month: 7, date: 16 },
   },
   {
     id: 9,
     name: "Rabi",
     interest: "Journalism",
-    imageurl:"https://alchetron.com/cdn/rabi-lamichhane-0aa12e8d-e20f-4275-96b3-39fef0ae61e-resize-750.jpeg",
+    imageurl:
+      "https://alchetron.com/cdn/rabi-lamichhane-0aa12e8d-e20f-4275-96b3-39fef0ae61e-resize-750.jpeg",
     dob: { year: 1974, month: 9, date: 13 },
   },
   {
     id: 10,
     name: "Rajesh",
     interest: "Salam/Falam",
-    imageurl:"https://pbs.twimg.com/media/EISlEQeW4AEbykd?format=jpg&name=medium",
+    imageurl:
+      "https://pbs.twimg.com/media/EISlEQeW4AEbykd?format=jpg&name=medium",
     dob: { year: 1964, month: 6, date: 9 },
   },
   {
     id: 11,
     name: "Chhabi Lal",
     interest: "Prachanda",
-    imageurl:"https://www.mikeldunham.com/.a/6a00d8341df99053ef00e55406ce018834-600wi",
+    imageurl:
+      "https://www.mikeldunham.com/.a/6a00d8341df99053ef00e55406ce018834-600wi",
     dob: { year: 1954, month: 12, date: 11 },
   },
   {
     id: 12,
     name: "Justin",
     interest: "Music",
-    imageurl:"https://sivtelegram.media/wp-content/uploads/2022/09/Six-facts-about-herpes-zoster-a-disease-that-affects-Justin.jpg",
+    imageurl:
+      "https://sivtelegram.media/wp-content/uploads/2022/09/Six-facts-about-herpes-zoster-a-disease-that-affects-Justin.jpg",
     dob: { year: 1994, month: 3, date: 1 },
   },
   {
     id: 13,
     name: "Akshya",
     interest: "Khiladi",
-    imageurl:"https://www.bollywoodhungama.com/wp-content/uploads/2022/11/Akshay-tour.jpeg",
+    imageurl:
+      "https://www.bollywoodhungama.com/wp-content/uploads/2022/11/Akshay-tour.jpeg",
     dob: { year: 1967, month: 9, date: 9 },
   },
   {
     id: 14,
     name: "Saif",
     interest: "Young",
-    imageurl:"https://images.firstpost.com/wp-content/uploads/2019/07/saif-ali-khan-380.jpg",
+    imageurl:
+      "https://images.firstpost.com/wp-content/uploads/2019/07/saif-ali-khan-380.jpg",
     dob: { year: 1970, month: 8, date: 16 },
   },
   {
     id: 15,
     name: "Pradeep",
     interest: "Prem Geet",
-    imageurl:"https://i0.wp.com/en.thecinematimes.com/wp-content/uploads/2022/07/Pradip-Khadka-Prakash-Looks.jpg",
+    imageurl:
+      "https://i0.wp.com/en.thecinematimes.com/wp-content/uploads/2022/07/Pradip-Khadka-Prakash-Looks.jpg",
     dob: { year: 1992, month: 3, date: 10 },
   },
   {
     id: 16,
     name: "Ronaldo",
     interest: "Football",
-    imageurl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQGEhNNQRzE6D79lhFT5_xNIDrInWgZTHO5gs-eiR_xN7BQDfumyLS5RV0FHnfUNvj7ZU&usqp=CAU",
+    imageurl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQGEhNNQRzE6D79lhFT5_xNIDrInWgZTHO5gs-eiR_xN7BQDfumyLS5RV0FHnfUNvj7ZU&usqp=CAU",
     dob: { year: 1976, month: 9, date: 18 },
   },
   {
     id: 17,
     name: "Felix",
     interest: "Bro Fist",
-    imageurl:"https://pbs.twimg.com/media/EWYhs_YXYAIBdva.jpg",
+    imageurl: "https://pbs.twimg.com/media/EWYhs_YXYAIBdva.jpg",
     dob: { year: 1989, month: 10, date: 24 },
   },
   {
     id: 18,
     name: "Mark",
     interest: "I'm CEO, Bitch.",
-    imageurl:"https://i0.wp.com/www.badchix.com/wp-content/uploads/2015/05/seen-on-badchix-mark-zuckerberg-back-in-his-harvard-years-17-photos-16.jpg?resize=585%2C500&ssl=1",
+    imageurl:
+      "https://i0.wp.com/www.badchix.com/wp-content/uploads/2015/05/seen-on-badchix-mark-zuckerberg-back-in-his-harvard-years-17-photos-16.jpg?resize=585%2C500&ssl=1",
     dob: { year: 1984, month: 5, date: 14 },
   },
   {
     id: 19,
     name: "Sanna Mirella",
     interest: "Prime Minister",
-    imageurl:"https://connectgujarat.com/wp-content/uploads/2019/12/fgfgfg-2.jpg",
+    imageurl:
+      "https://connectgujarat.com/wp-content/uploads/2019/12/fgfgfg-2.jpg",
     dob: { year: 1985, month: 11, date: 16 },
   },
   {
     id: 20,
     name: "Anuradha Koirala",
     interest: "Social",
-    imageurl:"https://upload.wikimedia.org/wikipedia/commons/6/65/Anuradha.jpg",
+    imageurl:
+      "https://upload.wikimedia.org/wikipedia/commons/6/65/Anuradha.jpg",
     dob: { year: 1949, month: 4, date: 14 },
   },
   {
     id: 21,
     name: "Elon",
     interest: "New Rocket Man",
-    imageurl:"https://th.bing.com/th/id/R.e3305fc859f799ef2c8239df1d67a715?rik=V4cVPiU1rRkL4Q&pid=ImgRaw&r=0",
+    imageurl:
+      "https://th.bing.com/th/id/R.e3305fc859f799ef2c8239df1d67a715?rik=V4cVPiU1rRkL4Q&pid=ImgRaw&r=0",
     dob: { year: 1971, month: 6, date: 28 },
   },
   {
     id: 22,
     name: "Salah",
     interest: "Egyptian King",
-    imageurl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQ_L6qSxAyMWyNC0bLGf7ef8YJCSeUvwSwHdAiy-2cTegL39errrfseWpLWuy4Bx-SNZU&usqp=CAU",
+    imageurl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQ_L6qSxAyMWyNC0bLGf7ef8YJCSeUvwSwHdAiy-2cTegL39errrfseWpLWuy4Bx-SNZU&usqp=CAU",
     dob: { year: 1992, month: 6, date: 15 },
   },
   {
     id: 23,
     name: "Zlatan",
     interest: "The Lion",
-    imageurl:"https://64.media.tumblr.com/3def3b59112a021e6b40afd77ba0cdc9/tumblr_nx3jlbiF8P1r3kdsdo1_640.jpg",
+    imageurl:
+      "https://64.media.tumblr.com/3def3b59112a021e6b40afd77ba0cdc9/tumblr_nx3jlbiF8P1r3kdsdo1_640.jpg",
     dob: { year: 1981, month: 10, date: 3 },
   },
   {
     id: 24,
     name: "Johnny",
     interest: "Captain Jack Sparrow",
-    imageurl:"https://th.bing.com/th/id/OIP.PbF3WezM1nKlzuZN0F9WrAAAAA?pid=ImgDet&rs=1",
+    imageurl:
+      "https://th.bing.com/th/id/OIP.PbF3WezM1nKlzuZN0F9WrAAAAA?pid=ImgDet&rs=1",
     dob: { year: 1963, month: 6, date: 9 },
   },
   {
     id: 25,
     name: "Amrit",
     interest: "All Rounder",
-    image:"amrit.jpg",
+    image: "amrit.jpg",
     dob: { year: 1988, month: 7, date: 10 },
   },
   {
     id: 26,
     name: "Aljan",
     interest: "Gorkhali Tiger",
-    image:"aljan.jpg",
+    image: "aljan.jpg",
     dob: { year: 1995, month: 3, date: 18 },
   },
-   {
+  {
     id: 27,
     name: "Dinesh",
     interest: "Germany",
@@ -195,7 +212,11 @@ export default [
     image: "narayan.jpg",
     dob: { year: 1987, month: 7, date: 8 },
   },
-
-  
-  
+  {
+    id: 29,
+    name: "Alibaba",
+    interest: "Love Life",
+    imageurl:'https://picsum.photos/200',
+    dob: { year: 2017, month: 6, date: 25 },
+  },
 ];
